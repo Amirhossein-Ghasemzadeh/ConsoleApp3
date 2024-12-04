12714091e8df9048017a0fa038c27866bcce3b44
Amirhossein Ghasemzadeh <amirghasemzadeh17@gmail.com>
2024-12-04 14:33:07 +0330

    add filtering option to student List

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -18,7 +18,8 @@ public class Student
 
 public class StudentManager
 {
-    private List<Student> students;
+    
+    public List<Student> students;
 
     public StudentManager()
     {
@@ -31,12 +32,29 @@ public class StudentManager
         students.Add(student);
     }
 
-    // Save students to a JSON file
-    public void SaveToFile(string fileName)
+     // Filter students by id
+     public IEnumerable<Student> FilterStudentsById(int id)
     {
+        return students.Where(s => s.Id.Equals(id));
+    }
+
+
+
+    // Save students to a JSON file
+    public void SaveToFile(List<Student> students, string fileName)
+    {
+        string filePath = $@"\\DESKTOP-328MO15\Users\Win10\Desktop\{fileName}";
         string jsonData = JsonSerializer.Serialize(students, new JsonSerializerOptions { WriteIndented = true });
-        File.WriteAllText(fileName, jsonData);
-        Console.WriteLine($"Students data saved to {fileName}");
+
+        try
+        {
+            File.WriteAllText(filePath, jsonData);
+            Console.WriteLine($"Students data saved to {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error saving file: {ex.Message}");
+        }
     }
 }
 
@@ -48,9 +66,34 @@ class Program
         StudentManager studentManager = new StudentManager();
 
         // Add students
-        studentManager.AddStudent(new Student(id : 1, name : "Alice", age : 20));
+        studentManager.AddStudent(new Student(id : 0, name : "mamad", age : 0));
+        studentManager.AddStudent(new Student(id : 1, name : "asgar", age : 1));
+        studentManager.AddStudent(new Student(id : 2, name : "jafar", age : 2));
 
+        // Filter Student
+       Console.WriteLine("Do u want find a specefic user?(YES/No)");
+       var findByIdInput = Convert.ToString(Console.ReadLine());
+       
+
+        if (findByIdInput.Equals("yes", StringComparison.OrdinalIgnoreCase))
+
+        {
+            Console.WriteLine("INSERT student Id:");
+            var selectedId = Convert.ToInt32( Console.ReadLine());
+            var filteredStudents = studentManager.FilterStudentsById(selectedId).ToList();
+            Console.WriteLine(filteredStudents.Count());
+            if (filteredStudents.Count()>0)
+            {
+                studentManager.SaveToFile(filteredStudents, "students.json");
+            } else
+            {
+                Console.WriteLine("No students found with the selected Id.");
+            }
+        } else
+        {
         // Save to file
-        studentManager.SaveToFile("students.json");
+        studentManager.SaveToFile(studentManager.students, "students.json");
+
+        }
     }
 }
